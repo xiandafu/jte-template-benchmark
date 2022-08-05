ced4d870b3f6c3a9a4048383791df16801a9fc1a
Andreas Hager <andy@mazebert.com>
2022-08-05 07:39:42 +0200

    Reduce amount of benchmark executions

--- src/main/java/com/mitchellbosecke/benchmark/BaseBenchmark.java
+++ src/main/java/com/mitchellbosecke/benchmark/BaseBenchmark.java
@@ -15,9 +15,9 @@ import org.openjdk.jmh.annotations.Warmup;
 
 import com.mitchellbosecke.benchmark.model.Stock;
 
-@Fork(5)
-@Warmup(iterations = 5)
-@Measurement(iterations = 10)
+@Fork(1)
+@Warmup(iterations = 1)
+@Measurement(iterations = 5)
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Benchmark)
